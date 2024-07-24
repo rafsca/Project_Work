aacedf1c51a492d33312700ff80f340b8fcb0aeb
Mx444 <mybbtester@gmail.com>
2024-07-24 11:08:50 +0200

    feat: routes user , utils, bcrypt

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from "express";
+import { handleErr } from "./utils/handleError";
+import { routerUser } from "./routers/users";
 
 const app = express();
 const server = express.json();
+
 const port = process.env.PORT || 3000;
 const baseURL = process.env.BASE_URL || "http://localhost";
 
@@ -11,6 +14,8 @@ app.get("/", async function (req: Request, res: Response) {
   return res.status(200).send("Hello World!");
 });
 
+app.use("/api/auth", routerUser);
+
 app.listen(port, function () {
-  console.log(`Server is running on ${baseURL}:${port}`);
+  console.log(`🚀 Server is running on ${baseURL}:${port}`);
 });
